53a4acef4b4d643f00b1f729a68736d2bee93e39
Elamin Elsayed <eelsaye@emory.edu>
2025-06-30 03:16:16 +0000

    Add database integration with SQL=Alchemy and save API data

--- project1.py
+++ project1.py
@@ -1,9 +1,12 @@
 import requests
+import pandas as pd
+import sqlalchemy as db
 
 url = "http://api.alquran.cloud/v1/ayah/262/en.asad"
 response = requests.get(url)
 
 data = response.json()
+
 
 if data["status"] == "OK":
   ayah = data["data"]
@@ -12,3 +15,26 @@ if data["status"] == "OK":
   print("Text:", ayah["text"])
 else:
   print("Error:", data["status"])
+
+
+if data["status"] == "OK":
+    ayah = data["data"]
+    ayah_dict = {
+        "surah": [ayah["surah"]["englishName"]],
+        "ayah_number": [ayah["numberInSurah"]],
+        "text": [ayah["text"]],
+        "translator": [ayah["edition"]["englishName"]]
+    }
+
+    df = pd.DataFrame(ayah_dict)
+    print(df)
+else:
+    print("Error:", data["status"])
+
+engine = db.create_engine('sqlite:///quran.db')
+
+df.to_sql('ayahs', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM ayahs;")).fetchall()
+   print(pd.DataFrame(query_result))
